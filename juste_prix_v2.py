fc2f65d67fdd4be7506bae306992e2b3a85cde16
Sebastien Baeyens <sesebaey@outlook.be>
2022-02-22 11:38:50 +0100

    ajout_du_nombre_de_tentatives.py

--- juste_prix_v2.py
+++ juste_prix_v2.py
@@ -2,11 +2,13 @@ from random import randint
 print("Bonjour et bienvenue au juste prix")
 prix = randint(1, 100)
 tentative = 10
+nombre_de_tentative = 0
 reponse = int(
     input(f"Entrer un prix entre 1 et 100 vous avez {tentative} tentative : "))
 
 while prix != reponse:
     tentative -= 1
+    nombre_de_tentative = nombre_de_tentative+1
     print(f"Il vous reste {tentative}")
     if prix > reponse:
         print(f"Plus grande que {reponse}")
@@ -18,4 +20,5 @@ while prix != reponse:
         print(f"Partie terminée la bonne réponse était {prix}")
         exit()
 if prix == reponse:
-    print(f"Bravo bien joué le juste prix est de {prix}")
+    print(
+        f"Bravo bien joué, il vous a fallu {nombre_de_tentative+1} pour touvez le juste prix est de {prix}")
