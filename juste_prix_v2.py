88bcff69dc4c98dcfa167ed46eef6b5fdb23d374
Sebastien Baeyens <sesebaey@outlook.be>
2022-02-22 11:18:11 +0100

    first commit

---
+++ juste_prix_v2.py
@@ -0,0 +1,21 @@
+from random import randint
+print("Bonjour et bienvenue au juste prix")
+prix = randint(1, 100)
+tentative = 10
+reponse = int(
+    input(f"Entrer un prix entre 1 et 100 vous avez {tentative} tentative : "))
+
+while prix != reponse:
+    tentative -= 1
+    print(f"Il vous reste {tentative}")
+    if prix > reponse:
+        print(f"Plus grande que {reponse}")
+        reponse = int(input("Entrer un prix: "))
+    else:
+        print(f"Plus petit que {reponse}")
+        reponse = int(input("Entrer un prix: "))
+    if tentative == 1:
+        print(f"Partie terminée la bonne réponse était {prix}")
+        exit()
+if prix == reponse:
+    print(f"Bravo bien joué le juste prix est de {prix}")
