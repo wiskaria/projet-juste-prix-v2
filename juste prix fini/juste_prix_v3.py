0e2e43fca1d42aaa0851c8a2260d6598615c3206
Sebastien Baeyens <sesebaey@outlook.be>
2022-03-04 00:49:20 +0100

    dslpasfini.py

--- juste prix fini/juste_prix_v3.py
+++ juste prix fini/juste_prix_v3.py
@@ -1,3 +1,6 @@
+#  désoler je n'est pas put finir a temps
+
+
 from pickle import TRUE
 from random import randint
 print("Bonjour et bienvenue au juste prix")
@@ -26,38 +29,41 @@ while TRUE:
                 f"Bravo bien joué, il vous a fallu {tentative_nv_1+1} de tentatives pour touvez le juste prix est de {prix}")
             exit()
     elif niveaux == 2:
-        nombre_de_tentative_nv2 = 0
-        prix = randint(1, 100)
-        reponse = int(
+        prix_nv2 = randint(1, 100)
+        reponse_nv2 = int(
             input(f"Entrer un prix entre 1 et 100 vous avez 10 tentative : "))
-        nombre_de_tentative_nv2 = nombre_de_tentative_nv2+1
-        print(f"Vous avez {nombre_de_tentative_nv2}/10 tentatives")
-        if prix > reponse:
-            print(f"Plus grande que {reponse}")
-            reponse = int(input("Entrer un prix: "))
-        else:
-            print(f"Plus petit que {reponse}")
-            reponse = int(input("Entrer un prix: "))
-        if nombre_de_tentative_nv2 == 10:
-            print(f"Partie terminée la bonne réponse était {prix}")
-    if prix == reponse:
-        print(
-            f"Bravo bien joué, il vous a fallu {nombre_de_tentative_nv2+1} pour touvez le juste prix est de {prix}")
+        nombre_de_tentative_nv2 = 10
+
+        while reponse_nv2 != prix_nv2:
+            nombre_de_tentative_nv2 = nombre_de_tentative_nv2+1
+            print(f"Vous avez {nombre_de_tentative_nv2}/10 tentatives")
+            if prix_nv2 > reponse_nv2:
+                print(f"Plus grande que {reponse_nv2}")
+                reponse_nv2 = int(input("Entrer un prix: "))
+            elif prix_nv2 < reponse_nv2:
+                print(f"Plus petit que {reponse_nv2}")
+                reponse_nv2 = int(input("Entrer un prix: "))
+        if nombre_de_tentative_nv2 == 0:
+            print(f"Partie terminée la bonne réponse était {prix_nv2}")
+            exit()
+        if prix_nv2 == reponse_nv2:
+            print(
+                f"Bravo bien joué, il vous a fallu {nombre_de_tentative_nv2+1} pour touvez le juste prix est de {prix_nv2}")
     elif niveaux == 3:
         maxi_choi = int(input("Choisissez  maximal"))
-        prix = randint(1, maxi_choi)
-        reponse = int(
+        prix_nv3 = randint(1, maxi_choi)
+        reponse_nv3 = int(
             input(f"Entrer un prix entre 1 et 100 vous avez 10 tentative : "))
         nombre_de_tentative = int(input("Entre un nombre de tentative :  "))
         tentative = nombre_de_tentative
-        if prix > reponse:
-            print(f"Plus grande que {reponse}")
-            reponse = int(input("Entrer un prix: "))
+        if prix_nv3 > reponse_nv3:
+            print(f"Plus grande que {reponse_nv3}")
+            reponse_nv3 = int(input("Entrer un prix: "))
         else:
-            print(f"Plus petit que {reponse}")
-            reponse = int(input("Entrer un prix: "))
-    if prix == reponse:
-        print(
-            f"Bravo bien joué, il vous a fallu {nombre_de_tentative+1} pour touvez le juste prix est de {prix}")
-    if tentative == 0:
-        print(f"Partie terminée la bonne réponse était {prix}")
+            print(f"Plus petit que {reponse_nv3}")
+            reponse_nv3 = int(input("Entrer un prix: "))
+        if prix_nv3 == reponse_nv3:
+            print(
+                f"Bravo bien joué, il vous a fallu {nombre_de_tentative+1} pour touvez le juste prix est de {prix_nv3}")
+        if tentative == 0:
+            print(f"Partie terminée la bonne réponse était {prix_nv3}")
